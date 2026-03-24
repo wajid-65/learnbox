11e1f36b9a17149ef4d0aaeef327f77d653708aa
Abdul Wajid <you@example.com>
2026-03-24 13:13:03 +0530

    Add Netlify _redirects for React Router

--- admin-portal/src/api/axios.js
+++ admin-portal/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://learnbox-ype6.onrender.com',
+  baseURL: 'https://learnbox-ype6.onrender.com/api',
   withCredentials: true,
 });
 
